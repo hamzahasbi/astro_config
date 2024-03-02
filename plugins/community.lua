8a6e1086cb53e791923a6d913b0fb1a5b9b51ca8
Hamza Hasbi <hamza.hasbi@gmail.com>
2024-03-02 20:54:34 +0100

    Add: WP neovim journey

--- plugins/community.lua
+++ plugins/community.lua
@@ -298,4 +298,62 @@ return {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "prettierd") end,
   },
+  {
+    "xeluxee/competitest.nvim",
+    dependencies = "MunifTanjim/nui.nvim",
+    config = function()
+      require("competitest").setup {
+        companion_port = 10045,
+        compile_command = {
+          cpp = { exec = "g++-13", args = { "$(FNAME)", "-O2 -std=c++17", "$(FNOEXT)" } },
+        },
+        run_command = {
+          cpp = { exec = "./$(FNOEXT)" },
+          js = { exec = "node", args = { "$(FNAME)" } },
+          bun = { exec = "bun", args = { "${FNAME}" } },
+        },
+      }
+    end,
+    lazy = false,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      handlers = {
+        -- for prettier
+        prettier = function()
+          require("null-ls").register(require("null-ls").builtins.formatting.prettier.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+            end,
+          })
+        end,
+
+        -- for prettierd
+        prettierd = function()
+          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+            end,
+          })
+        end,
+        -- For eslint_d:
+        eslint_d = function()
+          require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".eslintrc.json"
+                or utils.root_has_file ".eslintrc.js"
+            end,
+          })
+        end,
+      },
+    },
+  },
 }
